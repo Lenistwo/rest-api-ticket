c9638e65191e1514d73318a15805e4c6f15366bb
Kacper <kacperek1003@gmail.com>
2019-09-13 13:38:15 +0200

    Performed Unit Tests

--- src/test/java/pl/lenistwo/rest/RestApplicationTests.java
+++ src/test/java/pl/lenistwo/rest/RestApplicationTests.java
@@ -1,16 +1,25 @@
 package pl.lenistwo.rest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import pl.lenistwo.rest.controller.TicketController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class RestApplicationTests {
+
+    @Autowired
+    private TicketController ticketController;
 
     @Test
     public void contextLoads() {
+        Assert.assertNotNull(ticketController);
     }
 
 }
